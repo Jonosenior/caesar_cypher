5d08dd54a13b9ccac832a70160fa8b8318c8e876
jonathan senior <jonosenior@gmail.com>
2018-01-23 12:45:38 +0100

    Refactored to remove superfluous route handler

--- cypher_app.rb
+++ cypher_app.rb
@@ -5,13 +5,13 @@ require 'sinatra/reloader'
 require 'erb'
 require_relative 'lib/caesar_cypher'
 
-get '/' do
-  erb :encryption
-end
+# get '/' do
+#   erb :encryption
+# end
 
-get '/encryption' do
+get '/' do
   @text = params["text"]
   @key = params["shift"].to_i
-  @encoded_text = caesar_cypher(@text, @key)
+  @encoded_text = caesar_cypher(@text, @key) if @text && @key
   erb :encryption
 end
